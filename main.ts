2f522073b0828e4c1e0be249f021e0d4623dba1c
OGhantous <122539542+OGhantous@users.noreply.github.com>
2023-01-20 08:44:46 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ input.onButtonPressed(Button.A, function () {
     basic.showIcon(Checkmark)
 })
 input.onSound(DetectedSound.Loud, function () {
-    basic.showIcon(IconNames.Happy)
+    basic.showIcon(Mad)
 })
 input.onButtonPressed(Button.AB, function () {
     basic.showIcon(Smile)
@@ -11,8 +11,19 @@ input.onButtonPressed(Button.B, function () {
     basic.showIcon(X)
 })
 input.onGesture(Gesture.Shake, function () {
-	
+    RPS = randint(1, 4)
+    if (RPS == 1) {
+        basic.showString("1")
+    } else if (RPS == 2) {
+        basic.showString("2")
+    } else if (RPS == 3) {
+        basic.showString("3")
+    } else {
+        basic.showString("4")
+    }
 })
+let RPS = 0
 let Checkmark = IconNames.Yes;
 let X = IconNames.No;
 let Smile = IconNames.Happy;
+let Mad = IconNames.Angry;
